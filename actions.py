7dfbf55bf3d4522985377e723704e840ffed8b7b
akash prasher <prince.akash.prasher@gmail.com>
2020-12-22 17:32:10 +0530

    Formating pip8

--- actions.py
+++ actions.py
@@ -1,11 +1,13 @@
-# File Containg Required Method/Function for program
+# File Containg Required Method/Function for todo.py
 
 import os
 from os import path
 from datetime import date
 
-#1 - Function Help - Show Commands
-def help():
+# 1 - Function Help - Show Commands
+
+
+def help_cmd():
     print("Usage :-")
     print("$ ./todo add \"todo item\"  # Add a new todo")
     print('$ ./todo ls               # Show remaining todos')
@@ -14,7 +16,9 @@ def help():
     print('$ ./todo help             # Show usage')
     print('$ ./todo report           # Statistics')
 
-#2 Adding New Todo -- ./todo add arg1 arg2
+# 2 Adding New Todo -- ./todo add arg1 arg2
+
+
 def add(todo_add):
     # Writing in todo.txt
     todo_size = len(todo_add)
@@ -22,52 +26,61 @@ def add(todo_add):
         f = open("todo.txt", "a")
         for i in range(2, todo_size):
             f.write(todo_add[i] + "\n")
-            print('Added todo: \"' + todo_add[i] +'\"')
+            print('Added todo: \"' + todo_add[i] + '\"')  # acknowledgement
         f.close()
     else:
-        print('Error: Missing todo string. Nothing added!')
+        print('Error: Missing todo string. Nothing added!')  # error
 
-#3 List all todos -- ./todo ls
+# 3 List all todos -- ./todo ls
+
+
 def list_todo():
-    if path.exists("todo.txt"):
-        todo_file = open("todo.txt","r")
+    if path.exists("todo.txt"):  # checking for the todo file
+        todo_file = open("todo.txt", "r")
         file_content = todo_file.read()
         final_list = file_content.split("\n")
         number_of_lines = len(final_list) - 1
         final_list.reverse()
         index_inverse = number_of_lines
-        for i in  range(1, number_of_lines + 1):
+        # loop for printing all todo exists in todo.txt
+        for i in range(1, number_of_lines + 1):
             print("[" + str(index_inverse) + "] " + final_list[i])
             index_inverse -= 1
         todo_file.close
     else:
-        print('There are no pending todos!')
+        print('There are no pending todos!')  # error
+
+# 4 Delete Todo
 
 
 def delete_todo(del_number):
     # Getting Todos
-    todo_file = open("todo.txt","r")
+    todo_file = open("todo.txt", "r")
     file_content = todo_file.read()
     final_list = file_content.split("\n")
     todo_file.close
 
     # Deleting Todo
     if len(final_list) > del_number > 0:
-        todo_file = open("todo.txt","w")
+        todo_file = open("todo.txt", "w")
         for todo in final_list:
             if todo != str(final_list[del_number - 1]):
                 if todo == '':
                     continue
                 else:
                     todo_file.write(todo + "\n")
-        print('Deleted todo #' + str(del_number))
+        print('Deleted todo #' + str(del_number))  # acknowledgement
         todo_file.close
     else:
-        print('Error: todo #' + str(del_number) + ' does not exist. Nothing deleted.')
-    
+        print('Error: todo #' + str(del_number) +
+              ' does not exist. Nothing deleted.')  # error
+
+# 5 Done todo and shifting to done.txt from todo.txt
+
+
 def done_todo(done_number):
     # Getting Todos
-    todo_file = open("todo.txt","r+")
+    todo_file = open("todo.txt", "r+")
     file_content = todo_file.read()
     final_list = file_content.split("\n")
     todo_file.close
@@ -76,33 +89,38 @@ def done_todo(done_number):
     if len(final_list) > done_number > 0:
         today = date.today()
         done_file = open("done.txt", "a")
-        done_file.write("x " + str(today.strftime("%Y-%m-%d")) + " " + str(final_list[done_number - 1]) + "\n")
-        print('Marked todo #' + str(done_number) +' as done.')
+        done_file.write("x " + str(today.strftime("%Y-%m-%d")) +
+                        " " + str(final_list[done_number - 1]) + "\n")
+        print('Marked todo #' + str(done_number) +
+              ' as done.')  # acknowledgement
         done_file.close
-        todo_file = open("todo.txt","w")
+        todo_file = open("todo.txt", "w")
         for todo in final_list:
             if todo != str(final_list[done_number - 1]):
                 if todo == '':
                     continue
                 else:
                     todo_file.write(todo + "\n")
-        print('Deleted todo #' + str(done_number))
+        print('Deleted todo #' + str(done_number))  # acknowledgement
         todo_file.close
     else:
-        print('Error: todo #' + str(done_number) + ' does not exist.')
+        print('Error: todo #' + str(done_number) + ' does not exist.')  # error
 
-# Telly Report
+# 6 Telly Report
+
+
 def report():
+    # Getting Remainning Todo
     today = date.today()
-    todo_file = open("todo.txt","r+")
+    todo_file = open("todo.txt", "r+")
     file_content = todo_file.read()
     pending = file_content.split("\n")
     todo_file.close
-
-    todo_file = open("done.txt","r+")
+    # Getting Completed Todo
+    todo_file = open("done.txt", "r+")
     file_content = todo_file.read()
     completed = file_content.split("\n")
     todo_file.close
-
-    print(str(today.strftime("%Y-%m-%d")) + " Pending : " + str(len(pending) - 1) + " Completed : " + str(len(completed) - 1))
-    # print(2020-12-22 Pending : 1 Completed : 2)
+    # Printing Report as per format
+    print(str(today.strftime("%Y-%m-%d")) + " Pending : " +
+          str(len(pending) - 1) + " Completed : " + str(len(completed) - 1))
--- todo.py
+++ todo.py
@@ -1,30 +1,37 @@
-# Importing Function/Method Required
+# importing sys for getting argument
 import sys
-from actions import help, add, list_todo, delete_todo, done_todo, report
+# Importing Function/Method Required from ./actions.py[required to run this file]
+from actions import help_cmd, add, list_todo, delete_todo, done_todo, report
 
 # checking number of arguments
 length_argument = len(sys.argv)
 
+# 1 cmd ./todo help
 if length_argument == 1 or sys.argv[1] == 'help':
-    help()
+    help_cmd()
 
+# 2 cmd ./todo add arg1 ...
 elif sys.argv[1] == 'add':
     add(sys.argv)
 
+# 3 cmd ./todo ls
 elif sys.argv[1] == 'ls':
     list_todo()
 
+# 4 cmd ./todo del arg1 ...
 elif sys.argv[1] == 'del':
     if len(sys.argv) > 2:
         delete_todo(int(sys.argv[2]))
     else:
         print('Error: Missing NUMBER for deleting todo.')
-        
+
+# 5 cmd ./todo done arg1...
 elif sys.argv[1] == 'done':
     if len(sys.argv) > 2:
         done_todo(int(sys.argv[2]))
     else:
         print('Error: Missing NUMBER for marking todo as done.')
 
+# 6 cmd ./todo report
 elif sys.argv[1] == 'report':
     report()
