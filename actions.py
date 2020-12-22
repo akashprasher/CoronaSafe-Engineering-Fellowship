b77f515b13ec9f17693b5c02d7ab5f63cba7866d
akash prasher <prince.akash.prasher@gmail.com>
2020-12-22 14:18:15 +0530

    Adding seprate file for fuctions
    Signed-off-by: akash prasher <prince.akash.prasher@gmail.com>

---
+++ actions.py
@@ -0,0 +1,11 @@
+# File Containg Required Method/Function for program
+
+#1 - Function Help
+def help():
+    print('Usage :-')
+    print('$ ./todo add "todo item"  # Add a new todo')
+    print('$ ./todo ls               # Show remaining todos')
+    print('$ ./todo del NUMBER       # Delete a todo')
+    print('$ ./todo done NUMBER      # Complete a todo')
+    print('$ ./todo help             # Show usage')
+    print('$ ./todo report           # Statistics')
